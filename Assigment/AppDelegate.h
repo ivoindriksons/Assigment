6589aaf74e55b874242ef20365481f8532df53d1
Ivo Indriksons <ivo.indriksons@fbmsoftware.com>
2016-02-05 15:05:40 +0200

    Initial Commit

---
+++ Assigment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assigment
+//
+//  Created by Ivo Indriksons on 05/02/16.
+//  Copyright © 2016 Ivo Indriksons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assigment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assigment
+//
+//  Created by Ivo Indriksons on 05/02/16.
+//  Copyright © 2016 Ivo Indriksons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
